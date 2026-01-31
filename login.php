79431889f1aca4afd8f4305b51cf0cca519a42ae
mara954 <m.mariano11@studenti.unisa.it>
2026-01-31 17:57:13 +0100

    prova

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
 session_start();
 
 // 1. Il "Buttafuori": se sei già loggato, vai alla home
